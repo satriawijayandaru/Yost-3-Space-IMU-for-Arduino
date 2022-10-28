befdd9e0f7041b77a0b645a1f80d770c91921a43
vrey11 <yanda1998@gmail.com>
2022-10-28 22:45:41 +0700

    update data preparation (not tested)

--- Reading_Yost_3D_Data/Reading_Yost_3D_Data.ino
+++ Reading_Yost_3D_Data/Reading_Yost_3D_Data.ino
@@ -1,4 +1,6 @@
-#define IMU Serial2
+#define ser1 Serial
+HardwareSerial IMU(PA3, PA2); //RX, TX
+HardwareSerial ser3(PB11, PB10); //RX, TX
 
 int led = 13;
 
@@ -12,17 +14,19 @@ int maxCSVIndex;
 char incomingByte;
 int incomingInt;
 
+float roll, pitch, yaw;
+
 void setup() {
   pinMode(led, OUTPUT);
-  Serial.begin(115200);
-  Serial2.begin(115200);
+  ser1.begin(115200);
+  IMU.begin(115200);
+  ser3.begin(115200);
   digitalWrite(led, HIGH);
 
 }
 
 void loop() {
   requestData(4, 100);
-
   if (IMU.available()) {
     serialIN(IMU.read(), 1);
   }
@@ -34,30 +38,44 @@ void requestData(int data, int interval) {
     previousMillis = currentMillis;
     switch (data) {
       case 1:
-        Serial.print("37(0x25) - ");
-        Serial.println("Get all corrected component sensor data");
+        ser1.print("37(0x25) - ");
+        ser1.println("Get all corrected component sensor data");
         IMU.println(":37");
         break;
 
       case 2:
-        Serial.println("38(0x26) - ");
-        Serial.println("Get corrected gyro rate");
+        ser1.println("38(0x26) - ");
+        ser1.println("Get corrected gyro rate");
         IMU.println(":38");
         break;
 
       case 3:
-        Serial.println("39(0x27) - ");
-        Serial.println("Get corrected accelerometer vector");
+        ser1.println("39(0x27) - ");
+        ser1.println("Get corrected accelerometer vector");
         IMU.println(":39");
         break;
 
       case 4:
-        Serial.println("1(0x01) - ");
-        Serial.println("Get tared orientation as euler angles");
+        ser1.println("1(0x01) - ");
+        ser1.println("Get tared orientation as euler angles");
         IMU.println(":1");
         break;
     }
   }
+}
+
+void dataPreparation(){
+  roll  = radToDeg(incomingCSV[2].toFloat());
+  pitch = radToDeg(incomingCSV[0].toFloat());
+  yaw   = radToDeg(incomingCSV[1].toFloat());
+  
+//  roll  = (incomingCSV[2].toFloat() * (180/ PI));
+//  pitch = (incomingCSV[0].toFloat() * (180/ PI));
+//  yaw   = (incomingCSV[1].toFloat() * (180/ PI));
+}
+
+float radToDeg(float data){
+  return data * (180/PI);
 }
 
 void serialIN(char incomingSerial, bool debugMode) {
@@ -67,10 +85,10 @@ void serialIN(char incomingSerial, bool debugMode) {
     int str_len = incomingString.length() - 1;
     incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
     if (debugMode == 1) {
-      Serial.print("Incoming ");
-      Serial.print(currentCSVIndex);
-      Serial.print(" = ");
-      Serial.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
+      ser1.print("Incoming ");
+      ser1.print(currentCSVIndex);
+      ser1.print(" = ");
+      ser1.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
     }
     currentCSVIndex++;
     incomingString = "";
@@ -80,14 +98,15 @@ void serialIN(char incomingSerial, bool debugMode) {
     incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
     maxCSVIndex = currentCSVIndex;
     if (debugMode == 1) {
-      Serial.print("Incoming ");
-      Serial.print(currentCSVIndex);
-      Serial.print(" = ");
-      Serial.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
-      Serial.print("max Index  = ");
-      Serial.println(maxCSVIndex);
-      Serial.println();
+      ser1.print("Incoming ");
+      ser1.print(currentCSVIndex);
+      ser1.print(" = ");
+      ser1.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
+      ser1.print("max Index  = ");
+      ser1.println(maxCSVIndex);
+      ser1.println();
     }
+    dataPreparation();
     incomingString = "";
     currentCSVIndex = 0;
   }
